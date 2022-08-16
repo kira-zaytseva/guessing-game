fb8e2c0aa8bc621b29b885f8a7479fe17510616b
Kira Zaytseva <kirazaytseva@192.168.1.38>
2022-08-17 02:45:59 +0300

    feat: realize game function

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,28 @@
 class GuessingGame {
-    constructor() {}
+    constructor(minValue, maxValue) {
+        this.minValue;
+        this.maxValue;
+        this.num = 0;
+    }
 
     setRange(min, max) {
-
+        this.minValue = min;
+        this.maxValue = max;
+        this.num = 0;
     }
 
     guess() {
-
+        this.result = Math.round((this.maxValue + this.minValue)/2);
+        return this.result;
     }
 
     lower() {
-
+        this.maxValue = this.result;
     }
 
     greater() {
-
+        //this.num = Math.floor((this.maxValue - this.minValue)/2) + this.num;
+        this.minValue = this.result;
     }
 }
 
